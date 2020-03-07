1442ac9aa8b56c610ff6bfaf11ce9cbf26ce495a
MostafaOmar98 <mostafa.omar.m.s@gmail.com>
2020-03-07 10:01:58 +0200

    Redesign of system

---
+++ src/BankerSimulator.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class BankerSimulator {
+    ProcessesState state;
+
+    BankerSimulator()
+    {
+        state = null;
+    }
+
+    private static void printMenu()
+    {
+        System.out.print("What would you like to do?\n" +
+                "1- Enter new processes state\n" +
+                "2- Add request to current state\n" +
+                "0- Quit");
+    }
+
+
+    void run()
+    {
+        Scanner sc = new Scanner(System.in);
+        while(true)
+        {
+            int choice;
+            choice = sc.nextInt();
+        }
+    }
+
+
+}
